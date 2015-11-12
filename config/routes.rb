693978fb93ee1d7dc874fed2f6660e28003761a8
Fergus Barratt <fergus.barratt.12@ucl.ac.uk>
2015-11-12 01:51:58 +0000

    more styling, more pages, tofix: edit page has weird button

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'flashcards#index'
   get '/about' => 'static_pages#about'
   get '/contact' => 'static_pages#contact'
-  get '/new' => 'flashcards#new'
+  get '/signup' => 'users#new'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
