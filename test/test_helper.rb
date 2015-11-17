d0d3293047ffc45b630e811ddf48392dc51ef21f
Fergus Barratt <fergus.barratt.12@ucl.ac.uk>
2015-11-17 20:34:14 +0000

    persistent remembering

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
   def is_logged_in?
-    !session[:user_id].nil?
+    !(session[:user_id].nil?)
   end
   # Add more helper methods to be used by all tests here...
 end
