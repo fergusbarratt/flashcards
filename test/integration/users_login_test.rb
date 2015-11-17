e94cb9213f8ca06424412a078862e1a096b556d3
Fergus Barratt <fergus.barratt.12@ucl.ac.uk>
2015-11-17 15:59:35 +0000

    login with valid info finish

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -15,6 +15,13 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   end
   test "login with valid information" do
     get login_path
-    post login_path, session: 
+    post login_path, session: { email: @user.email, password: 'password' }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", login_path, count:0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
   end
 end
