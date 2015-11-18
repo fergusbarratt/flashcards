8971eaf685f8027c39b96282bd20ebba7de77b17
Fergus Barratt <fergus.barratt.12@ucl.ac.uk>
2015-11-18 21:45:08 +0000

    basic user security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:index, :destroy]
 
   def show
     @user = User.find(params[:id])
@@ -50,6 +50,7 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+    
     def logged_in_user
       unless logged_in?
         store_location
