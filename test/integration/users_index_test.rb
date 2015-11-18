012ca35e483d545a4625b1734168ff84904e5a6b
Fergus Barratt <fergus.barratt.12@ucl.ac.uk>
2015-11-18 21:15:12 +0000

    users list, admin users etc

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -2,12 +2,20 @@ require 'test_helper'
 
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:fergus)
+    @admin     = users(:fergus)
+    @non_admin = users(:archer)
   end
-
-  test "index" do
-    log_in_as(@user)
-    get users_path
+  test 'index as admin' do
+    log_in_as(@admin)
+    get users_path 
     assert_template 'users/index'
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+    end
+  end
+  test "index as nonadmin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select 'a', text: 'delete', count:0
   end
 end
