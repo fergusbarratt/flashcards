094ca7f30220fdb7658faf0961614cfaf9122df2
Fergus Barratt <fergus.barratt.12@ucl.ac.uk>
2015-11-18 00:02:57 +0000

    updating users now possible

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def show
     @user = User.find(params[:id])
   end
 
   def new
     @user = User.new
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "updated profile"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   def create
@@ -18,8 +31,22 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "log in to get to this page"
+        redirect_to login_url
+      end
+    end
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless @user == current_user
     end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -30,6 +30,10 @@ module SessionsHelper
     !current_user.nil?
   end
 
+  def current_user?(user)
+    user == current_user 
+  end
+
   # Forgets a persistent session.
   def forget(user)
     user.forget
